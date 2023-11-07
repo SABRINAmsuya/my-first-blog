6d02e611865306557c9028e1ea5689f3dc0eb57a
SABRINAmsuya <sabrinahmsuya04@gmail.com>
2023-11-07 09:38:20 -0800

    My Django girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r)(svs0pepw1havs+kjgbzyoqyg(^35q=8xi37p%jl7d5t=&ss'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1' , ' .pythonanywhere.com']
 
